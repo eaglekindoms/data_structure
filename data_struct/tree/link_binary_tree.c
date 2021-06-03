161166f7a609bd6d6cee5308c490bd5b7c9fe544
eaglekindoms <huyongmao35@outlook.com>
2021-06-03 21:47:49 +0800

    暂存

--- data_struct/tree/link_binary_tree.c
+++ data_struct/tree/link_binary_tree.c
@@ -4,23 +4,20 @@
 
 #include "binary_tree.h"
 
-typedef struct BNode
-{
+typedef struct BNode {
     T data;
     struct BNode *left;
     struct BNode *right;
 } Node;
 
-struct Tree_T
-{
+struct Tree_T {
     Node *root;
     int length;
 };
 
 // 初始化二叉树
-Tree initTree()
-{
-    Tree tree = (Tree)malloc(sizeof(Tree));
+Tree initTree() {
+    Tree tree = (Tree) malloc(sizeof(Tree));
     tree->root = NULL;
     tree->length = 0;
     return tree;
@@ -29,95 +26,104 @@ Tree initTree()
 // 查找父节点
 /**
  * if data==node.data return node
- * if node.
  * */
-Node *findParentNode(Tree tree, T data)
-{
-    if (tree->root == NULL)
-    {
+Node *findParentNode(Tree tree, T data) {
+    if (tree->root == NULL) {
         exit(ERROR_NULL_PTR);
     }
     Node *curr = tree->root;
-    while (data < curr->data)
-    {
-        if (curr->left == NULL)
-        {
+    while (curr != NULL) {
+        if (data > curr->data) {
+            if (curr->right == NULL)
+                return curr;
+            curr = curr->right;
+        } else if (data < curr->data) {
+            if (curr->left == NULL)
+                return curr;
+            curr = curr->left;
+        } else {
             return curr;
         }
-        else
-        {
-            curr = curr->left;
-        }
     }
-
-    return tree->root;
+    return NULL;
 }
 
 // 插入元素
-void putElem(Tree tree, T data)
-{
-    Node *node = (Node *)malloc(sizeof(Node));
-    if (tree->root == NULL)
-    {
+void putElem(Tree tree, T data) {
+    Node *node = (Node *) malloc(sizeof(Node));
+    node->data = data;
+    node->left = NULL;
+    node->right = NULL;
+    if (tree->root == NULL) {
         tree->length++;
         tree->root = node;
-    }
-    else
-    {
+    } else {
         Node *parent = findParentNode(tree, data);
-        if (data < parent->data)
-        {
+        if (data < parent->data) {
             parent->left = node;
-        }
-        else
-        {
+            tree->length++;
+        } else if (data > parent->data) {
             parent->right = node;
+            tree->length++;
         }
     }
 }
 
 // 移除元素
-void removeElem(Tree tree, T data)
-{
-    Node *parent = findParentNode(tree, data);
-    if (parent->data != data)
-    {
+void removeElem(Tree tree, T data) {
+    Node *node = findParentNode(tree, data);
+    if (node == NULL || node->data != data) {
         printf("\n---don't find element---\n");
         return;
     }
+    if (node->left == NULL && node->right == NULL) {
+        free(node);
+        node = NULL;
+    }
+    if (node->right == NULL && node->left != NULL) {
+        free(node);
+        node = node->left;
+    }
+    if (node->right != NULL && node->left == NULL) {
+        free(node);
+        node = node->right;
+    }
+
 }
 
 // 查找元素
-int searchElem(Tree tree, T data)
-{
+int searchElem(Tree tree, T data) {
 }
 
 // 获取树元素总量
-int getLength(Tree tree)
-{
+int getLength(Tree tree) {
 }
 
 // 获取树高
-int getHeight(Tree tree)
-{
+int getHeight(Tree tree) {
+}
+
+void preOrderNode(Node *node) {
+    if (node != NULL) {
+        printf("%d ", node->data);
+        preOrderNode(node->left);
+        preOrderNode(node->right);
+    }
 }
 
 // 先序遍历
-void preOrder(Tree tree)
-{
+void preOrder(Tree tree) {
+    preOrderNode(tree->root);
 }
 
 // 中序遍历
-void inOrder(Tree tree)
-{
+void inOrder(Tree tree) {
 }
 
 // 后序遍历
-void postOrder(Tree tree)
-{
+void postOrder(Tree tree) {
 }
 
 // 层序遍历
-void levelOrder(Tree tree)
-{
+void levelOrder(Tree tree) {
 }
--- data_struct/tree/test_binary.c
+++ data_struct/tree/test_binary.c
@@ -4,7 +4,14 @@
 
 #include "binary_tree.h"
 
-int test_binary_tree()
-{
+int test_binary_tree() {
+    Tree tree = initTree();
+    putElem(tree, 2);
+    putElem(tree, 42);
+    putElem(tree, 2);
+    putElem(tree, 24);
+    putElem(tree, 21);
+    removeElem(tree,222);
+    preOrder(tree);
     return 0;
 }

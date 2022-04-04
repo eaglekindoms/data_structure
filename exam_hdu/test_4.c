52798a8d0bfab2dc136d19709b2309436ce26079
eaglekindoms <huyongmao35@outlook.com>
2022-04-04 08:31:13 +0800

    add exam dir

---
+++ exam_hdu/test_4.c
@@ -0,0 +1,12 @@
+//
+// Created by eagle on 2022/4/4.
+//
+
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+
+int main() {
+
+    return 0;
+}
---
+++ interview/h_2_6.c
@@ -0,0 +1,23 @@
+//
+// Created by eagle on 2022/3/26.
+//
+
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+
+
+/*
+ * 1.
+ */
+int main() {
+    int n;
+    scanf("%d", &n);
+    while (n!=0){
+        int win;
+
+        printf("%d",win);
+        scanf("%d", &n);
+    }
+    return 0;
+}
